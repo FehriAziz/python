799b2530794067151751cf1aaf6926e2d3d177cb
FehriAziz <azizfehri1@gmail.com>
2023-07-10 16:53:38 +0100

    test

---
+++ 01-python-fundamental/lists.py
@@ -0,0 +1,32 @@
+x = [ [5,2,3], [10,8,9] ]
+students = [
+    {'first_name': 'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+# --------10 to 15-----
+
+# x[1][0]= 15
+# print (x)
+
+# #------- jordan to Bryant------
+
+# students[0]['last_name']= "Bryant"
+# print (students)
+
+# #------messi to andres
+# sports_directory['soccer'][0] = "andreas"
+# print (sports_directory)
+
+# #----20 to 30 
+# z[0]['y']= 30
+# print(z)
+
+
+
+#2. Iterate Through a List of Dictionaries
+
